55b795ab993416b7472f7ee0cdad97a055a49274
vale-hash <newlechim@gmail.com>
2024-04-20 20:18:00 +0100

    puppent lint for 1-install_a_package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,7 @@ package{'python3-pip':
 }
 
 package {'flask':
-  ensure    =>  '2.1.0',
-  provider  =>  'pip3',
-  require   =>  Package['python3-pip']
+  ensure   =>  '2.1.0',
+  provider =>  'pip3',
+  require  =>  Package['python3-pip']
 }
