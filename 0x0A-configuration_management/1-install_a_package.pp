67b78c3c23789f901744b838373422c0a638432f
vale-hash <newlechim@gmail.com>
2024-04-20 20:14:56 +0100

    ensure pip3 is installed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #script to install flask using pip3
 
+package{'python3-pip':
+  ensure => installed,
+}
+
 package {'flask':
-  ensure   =>  '2.1.0',
-  provider =>  'pip3',
+  ensure    =>  '2.1.0',
+  provider  =>  'pip3',
+  require   =>  Package['python3-pip']
 }
