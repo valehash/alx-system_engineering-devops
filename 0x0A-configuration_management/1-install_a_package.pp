0220694551fc5bbe6ad6fed0a7795b814e5505eb
vale-hash <newlechim@gmail.com>
2024-04-20 04:38:24 +0100

    created a puppet script that installs flask using pip3

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,21 @@
+$base_packages = {'python-pip', 'python-devel', 'openssl-devel', 'libffi-devel'}
+
+$package_dependencies = concatenate($base_packages, 'pip3')
+
+
+package{$base_packages:
+  ensure    =>  'installed'
+}
+
+package{'pip3':
+  ensure   =>  latest,
+  require  =>  Package[$base_packages],
+  provider =>  'pip3',
+}
+
+package {'flask':
+  ensure   =>  installed,
+  require  =>  Package[$package_dependencies],
+  name     =>  'flask~=2.1.0',
+  provider =>  'pip3',
+}
