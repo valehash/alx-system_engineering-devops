6e2efe1428cbe5a89dbef8ad34f780e99a1dd98d
vale-hash <newlechim@gmail.com>
2024-04-20 20:30:41 +0100

    added werkzurg

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,8 +4,17 @@ package{'python3-pip':
   ensure => installed,
 }
 
+package {'Werkzeug'
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip']
+}
+
+
+
 package {'Flask':
   ensure   =>  '2.1.0',
   provider =>  'pip3',
   require  =>  Package['python3-pip'],
 }
+
