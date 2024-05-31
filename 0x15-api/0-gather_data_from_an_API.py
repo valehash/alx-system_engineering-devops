60160147a2514599fa39d2aa290ec274210a4837
vale-hash <newlechim@gmail.com>
2024-05-31 17:45:58 +0100

    gather api data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-"""python script to learn restAPI"""
+"""Accessing a REST API for todo lists of employees"""
+
 import requests
 import sys
 
 
-def get_employee(resource):
-	emp_url = "https://jsonplaceholder.typicode.com/"
-	emp_url = emp_url + resource + sys.argv[1]
-	employee = requests.get(emp_url)
-	employee = employee.json()
-	employee_name = employee['name']
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-	task_url = f"https://jsonplaceholder.typicode.com/todos/?userId={sys.argv[1]}"
-	tasks = requests.get(task_url)
-	task = tasks.json()
-	print(len(task))
+    response = requests.get(url)
+    employeeName = response.json().get('name')
 
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
 
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
 
-if __name__ == "__main__":
-	get_employee("users/")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
