71989e40ed2ffb5eacea2e973655a74195902672
vale-hash <newlechim@gmail.com>
2024-06-04 18:00:19 +0100

    api prints 10 hot topics in any subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import requests
+def top_ten(subreddit):
+    """code to get the number of subscribers in a subreddit"""
+    
+    base_url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
+    headers = {'User-Agent': 'vale/1.0.0'}
+    
+    requested = requests.get(base_url, headers=headers, allow_redirects=False)
+
+    if requested.status_code != 200:
+        return 0
+
+    r_json = requested.json()
+    
+    data = r_json.get("data")
+
+    children = data.get("children")
+    #print(children)
+    for child in children[:10]:
+        hot = child.get("data")
+        print(hot.get("title"))
