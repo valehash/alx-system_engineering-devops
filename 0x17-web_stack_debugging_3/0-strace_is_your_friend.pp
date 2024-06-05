46226aa50d3377ccf3f9a477afe181b3b794c7db
vale-hash <newlechim@gmail.com>
2024-06-05 14:23:47 +0100

    replaced phpp with php

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,10 @@
 # fixing php with puppet
-
 file { '/var/www/html/wp-settings.php':
   ensure => present,
 }
 
 exec { 'replace_phpp_with_php':
-  command => 'sed -i \'s/\.phpp/\.php/g\' /var/www/html/wp-settings.php',
-  onlyif  => 'grep \.phpp /var/www/html/wp-settings.php',
+  command => '/bin/sed -i \'s/\.phpp/\.php/g\' /var/www/html/wp-settings.php',
+  onlyif  => '/bin/grep \.phpp /var/www/html/wp-settings.php',
   require => File['/var/www/html/wp-settings.php'],
 }
-
