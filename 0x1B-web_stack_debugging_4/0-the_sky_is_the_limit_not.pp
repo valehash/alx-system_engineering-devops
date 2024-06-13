7304946760e266bbb75da3b8a2cdde140b925821
vale-hash <newlechim@gmail.com>
2024-06-13 13:52:41 +0100

    removed only if command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -6,7 +6,7 @@ file { '/etc/default/nginx':
 
 exec { 'update_connection_limit':
   command => '/bin/sed -i \'s/ULIMIT="-n [0-9]*"/ULIMIT="-n 10000"/g\' /etc/default/nginx',
-  onlyif  => '/bin/grep "24: Too many open files" /var/log/nginx/error.log',
+  #onlyif  => '/bin/grep "24: Too many open files" /var/log/nginx/error.log',
   require => File['/etc/default/nginx'],
 }
 
