7025cd5b3923004e30d5d20d992f90db0186f7b9
vale-hash <newlechim@gmail.com>
2024-06-13 13:50:01 +0100

    up removed notif

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,5 @@
 #fixing nginx requests with puppet
+
 file { '/etc/default/nginx':
   ensure => present,
 }
@@ -7,7 +8,6 @@ exec { 'update_connection_limit':
   command => '/bin/sed -i \'s/ULIMIT="-n [0-9]*"/ULIMIT="-n 10000"/g\' /etc/default/nginx',
   onlyif  => '/bin/grep "24: Too many open files" /var/log/nginx/error.log',
   require => File['/etc/default/nginx'],
-  notify  => Exec['reload_nginx'],
 }
 
 exec { 'reload_nginx':
